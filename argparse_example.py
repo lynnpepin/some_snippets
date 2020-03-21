7205a7751280f5b47fe807ac7770cecbcebc7b36
lynnpepin <lynnpepin@tutanota.com>
2020-03-21 12:33:41 -0400

    Initial commit, added argparse example

---
+++ argparse_example.py
@@ -0,0 +1,37 @@
+# example: python argparse_example.py -s Hello -n 100 -b -l 1 10 2
+
+import argparse
+
+if __name__ == '__main__':
+    # Initialize argparser
+    parser = argparse.ArgumentParser(description="Put your description here")
+    
+    # Add arguments
+    parser.add_argument("--string_arg", "-s",
+                        help="String argument.",
+                        type=str, nargs=1)
+    parser.add_argument("--number_arg","-n",
+                        help="Integer argument.",
+                        type=int, nargs=1)
+    parser.add_argument("--list_arg","-l",
+                        help="A list of ints",
+                        type=int, nargs='+')  # use nargs='+' to require at least one value
+    parser.add_argument("--bool_arg","-b",
+                        help="Boolean argument.",
+                        action="store_true")
+
+    # Parse and access arguments
+    # string and numbers are given as lists
+    # bools are not
+    args = parser.parse_args()
+    s = args.string_arg[0]
+    l = args.list_arg
+    n = args.number_arg[0]
+    b = args.bool_arg
+    
+    print("Here you go!")
+    print("s:", s)
+    print("l:", l)
+    print("n:", n)
+    print("b:", b)
+    print("All done!")

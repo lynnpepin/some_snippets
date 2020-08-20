24f2fb9eaf2064ae844052b1ffed7139e412b293
Lynn Pepin <lynnpepin@tutanota.com>
2020-08-20 18:56:34 +0000

    Update fstring_example.py

--- fstring_example.py
+++ fstring_example.py
@@ -1,8 +1,31 @@
 print(f"This is an fstring! 1/7 is approximately {1/7:.3f}.")
-print(f"This is ticket number {3621:>12}")
-print(f"Uh oh, I meant number {3621:>012}")
+"""
+>>> print(f"This is an fstring! 1/7 is approximately {1/7:.3f}.")
+This is an fstring! 1/7 is approximately 0.143.
+"""
+
+print(f"This is ticket number {1025:>12}")
+"""
+>>> print(f"This is ticket number {1025:>12}")
+This is ticket number         1025
+# Note the whitespace 0123456789ab
+# ":>12" right-justifies the number in a 12-character space
+"""
+
+print(f"Uh oh, I meant number {1025:>012}")
+"""
+>>> print(f"Uh oh, I meant number {1025:>012}")
+Uh oh, I meant number 000000001025
+# Same as above, but fills the space with zeros!
+"""
 
 def double(x):
     return 2*x
 
-#print(f"If you have 3.8, let's self document: {double(3) = }")
+# New in Python 3.8: Self documentation with fstrings!
+print(f"If you have 3.8, let's self document: {double(3) = }")
+"""
+>>> print(f"If you have 3.8, let's self document: {double(3) = }")
+If you have 3.8, let's self document: double(3) = 6
+# Noticee the fstring filled in the value!
+"""

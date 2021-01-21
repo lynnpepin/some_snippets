c243a6a657e19542a95b9b7cf0125beaf78ce6d5
lynnpepin <lynnpepin@tutanota.com>
2021-01-21 13:43:39 -0500

    Adding my bad example code

---
+++ nested_path_import/all_my_code/utilities/this_imports_calculator.py
@@ -0,0 +1,4 @@
+from .calculator import addition
+
+def subtraction(x, y):
+    return addition(x, -y)
---
+++ nested_path_import/run_code.sh
@@ -0,0 +1,5 @@
+# runs all_my_code/main.py,
+# which imports  `subtraction`  from all_my_code/utilities/this_imports_calculator.py,
+# which itself imports `addition`  from  `calculator.py`
+
+python all_my_code/main.py

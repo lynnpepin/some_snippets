f62868f98f3caef170d4a6c8b75e09646da87988
lynnpepin <lynnpepin@tutanota.com>
2021-09-04 01:37:00 -0400

    Added lynnkit empty file

---
+++ lynnkit.py
@@ -0,0 +1,14 @@
+'''lynnkit.py
+
+A collection of various, unrelated functions that I use often.
+
+This is made by me for me, but you can use it too!
+'''
+
+# todo:
+
+# simple socket client, server
+
+# fib
+
+# normal map code

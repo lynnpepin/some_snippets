02548bab565ab8090247d48f99fe21ae83fd0a1e
Lynn Pepin <lynn.pepin@uconn.edu>
2022-01-14 13:33:20 -0500

    Add recursive flatten examples

--- lynnkit.py
+++ lynnkit.py
@@ -99,3 +99,87 @@ def equi_hash(n, a = 0.618033988749895, K = 131072):
     :rtype: int
     """
     return math.floor(((n * a) % 1)*K)
+
+
+
+def flatten(x):
+    """Recursively flatten x, where x can be:
+     * A dict where leaf values are array-like or other iterable:
+     * An array-like, or
+     * An int/float.
+
+    Useful for converting a model.grid_state dict to a raw numpy array.
+
+    Example usage:
+    flatten(model.grid_state)
+
+    # Warning - can take about 1GB of RAM for 1440 timesteps.
+    # will have 1441 elements.
+    timeline = np.stack([flatten(grid_state) for grid_state in model.grid_timeline])
+    """
+    if type(x) is np.ndarray:
+        return x
+    elif type(x) is dict:
+        return np.concatenate(np.array([flatten(x[k]) for k in x.keys()]))
+    elif type(x) in (list, tuple):
+        return np.concatenate(np.array([flatten(v) for v in x]))
+    elif type(x) is set:
+        return np.concatenate(np.array([flatten(v) for v in list(x)]))
+    elif type(x) in (float, int):
+        return np.array([float(x)])
+    elif type(x) is str:
+        return np.array([float(x)])
+
+
+
+# return a flat list of float, int, str
+# or none if the object has no indices
+
+def flatten_indices(x, sep = ' ', depth=0, max_depth=-1):
+    """Recursively flatten the indices of an iterable to a list of strings.
+
+    >>> flatten({'a' : 1, 'b' : [1, 2, [10, 20]], 'c'  : {'x' : 100, 'y' : 200}})
+    [1, 1, 2, 10, 20, 100, 200]
+    >>> flatten_indices(flatten({'a' : 1, 'b' : [1, 2, [10, 20]], 'c'  : {'x' : 100, 'y' : 200}}))
+    ['a', 'b 0', 'b 1', 'b 2 0', 'b 2 1', 'c x', 'c y']
+
+
+    :param x: An iterable or value.
+    :type x: dict | float | int | str | None
+    :param sep: Separator for string representation of keys, defaults to ' '
+    :type sep: str, optional
+    :param depth: Current depth, defaults to 0
+    :type depth: int, optional
+    :param max_depth: Maximum depth to record. Defaults to unbounded, defaults to -1
+    :type max_depth: int, optional
+    :return: List representing the indices used in X, recursive.
+    :rtype: list of str
+    """
+    
+    if depth == max_depth:
+        # Do not return anything deeper than this depth
+        return None
+    
+    if type(x) in (float, int, str):
+        return None
+    elif type(x) is dict:
+        indices = x.keys()
+    elif type(x) in (list, tuple):
+        indices = range(len(x))
+    
+    flattened_indices = []
+    
+    for idx in indices:
+        child = x[idx]
+        child_indices = flatten_indices(child, depth+1)
+        
+        if child_indices is None:
+            # child is float, int, or str
+            # so just add ur index to the flattened indices
+            flattened_indices.append(str(idx))
+        else:
+            # child is iterable
+            for child_idx in child_indices:
+                flattened_indices.append(str(idx) + sep + child_idx)
+    
+    return flattened_indices
